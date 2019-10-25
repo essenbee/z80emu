8165f48ea7f0489ffda603947dce761167ffa0dc
Stuart Bonham <stuart_bonham@blueyonder.co.uk>
2019-10-25 18:07:57 +0100

    Added reading of Expected file

--- Essenbee.Z80.Tests/Classes/FuseTester.cs
+++ Essenbee.Z80.Tests/Classes/FuseTester.cs
@@ -9,10 +9,18 @@ namespace Essenbee.Z80.Tests.Classes
     {
         private const string _testsFile = @"..\..\..\FuseTests\FUSE_Tests.txt";
         private const string _expectedFile = @"..\..\..\FuseTests\FUSE_Expected.txt";
-        private List<FuseTest> _tests = new List<FuseTest>();
+        private Dictionary<string, FuseTest> _tests = new Dictionary<string, FuseTest>();
+        private Dictionary<string, FuseExpected> _expected = new Dictionary<string, FuseExpected>();
         class FuseTest
         {
-            public string TestName { get; set; } = string.Empty;
+            public List<int> Registers { get; set; } = new List<int>();
+            public List<int> States { get; set; } = new List<int>();
+            public List<List<int>> Memory { get; set; } = new List<List<int>>();
+        }
+
+        class FuseExpected
+        {
+            public List<List<string>> Events { get; set; } = new List<List<string>>();
             public List<int> Registers { get; set; } = new List<int>();
             public List<int> States { get; set; } = new List<int>();
             public List<List<int>> Memory { get; set; } = new List<List<int>>();
@@ -24,6 +32,7 @@ namespace Essenbee.Z80.Tests.Classes
             var aTest = new FuseTest(); ;
             var newTest = true;
             var lineType = 1;
+            var testName = string.Empty;
 
             foreach (var line in lines)
             {
@@ -40,7 +49,7 @@ namespace Essenbee.Z80.Tests.Classes
 
                 if (line.StartsWith("-1"))
                 {
-                    _tests.Add(aTest);
+                    _tests.Add(testName, aTest);
                     newTest = true;
                     lineType = 1;
                     continue;
@@ -49,7 +58,7 @@ namespace Essenbee.Z80.Tests.Classes
                 if (lineType == 1)
                 {
                     // Test name
-                    aTest.TestName = line;
+                    testName = line;
                     lineType++;
                     continue;
                 }
@@ -93,6 +102,85 @@ namespace Essenbee.Z80.Tests.Classes
         public void ReadFuseExpectedFile()
         {
             var lines = File.ReadAllLines(_expectedFile);
+            var anExpected = new FuseExpected(); ;
+            var newExptected = true;
+            var lineType = 1;
+            var testName = string.Empty;
+
+            foreach (var line in lines)
+            {
+                if (line.Length == 0)
+                {
+                    _expected.Add(testName, anExpected);
+                    newExptected = true;
+                    lineType = 1;
+                    continue;
+                }
+
+                if (newExptected)
+                {
+                    anExpected = new FuseExpected();
+                    newExptected = false;
+                }
+
+
+                if (lineType == 1)
+                {
+                    // Test name
+                    testName = line;
+                    lineType++;
+                    continue;
+                }
+
+                if (lineType == 2)
+                {
+                    if (line.StartsWith(" "))
+                    {
+                        // Event
+                        var anEvent = line.Split(' ').ToList();
+                        anEvent.RemoveAll(x => x.Equals(string.Empty));
+
+                        anExpected.Events.Add(anEvent);
+                        continue;
+                    }
+
+                    lineType++;
+                }
+
+                if (lineType == 3)
+                {
+                    // Registers
+                    var regs = line.Split(' ').ToList();
+                    regs.RemoveAll(x => x.Equals(string.Empty));
+                    var regsHex = regs.Select(hex => Convert.ToInt32(hex, 16)).ToList();
+
+                    anExpected.Registers = regsHex;
+                    lineType++;
+                    continue;
+                }
+
+                if (lineType == 4)
+                {
+                    // State data
+                    var states = line.Split(' ').ToList();
+                    states.RemoveAll(x => x.Equals(string.Empty));
+                    var statesHex = states.Select(hex => Convert.ToInt32(hex, 16)).ToList();
+                    anExpected.States = statesHex;
+                    lineType++;
+                    continue;
+                }
+
+                if (lineType == 5)
+                {
+                    // Memory (1 or more records of this type)
+                    var mem = line.Split(' ').ToList();
+                    mem.RemoveAll(x => x.Equals(string.Empty));
+                    mem.RemoveAll(x => x.Equals("-1"));
+                    var memHex = mem.Select(hex => Convert.ToInt32(hex, 16)).ToList();
+                    anExpected.Memory.Add(memHex);
+                    continue;
+                }
+            }
         }
     }
 }
