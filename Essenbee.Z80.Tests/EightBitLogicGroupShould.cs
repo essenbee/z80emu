80acb5fa6c071f920191ba757e8a56518a3621b3
Stu Bonham <sbonham@rics.org>
2019-11-05 13:17:29 +0000

    Added test for XOR A zeroes A

--- Essenbee.Z80.Tests/EightBitLogicGroupShould.cs
+++ Essenbee.Z80.Tests/EightBitLogicGroupShould.cs
@@ -556,7 +556,7 @@ namespace Essenbee.Z80.Tests
             var program = new Dictionary<ushort, byte>
             {
                 // Program Code
-                { 0x0080, 0xDD }, // OR (IX+3)
+                { 0x0080, 0xDD }, // XOR (IX+3)
                 { 0x0081, 0xAE },
                 { 0x0082, 0x03 },
                 { 0x0083, 0x00 },
@@ -604,7 +604,7 @@ namespace Essenbee.Z80.Tests
             var program = new Dictionary<ushort, byte>
             {
                 // Program Code
-                { 0x0080, 0xFD }, // OR (IY+3)
+                { 0x0080, 0xFD }, // XOR (IY+3)
                 { 0x0081, 0xAE },
                 { 0x0082, 0x03 },
                 { 0x0083, 0x00 },
@@ -642,6 +642,40 @@ namespace Essenbee.Z80.Tests
             Assert.False((cpu.F & Z80.Flags.C) == Z80.Flags.C);
             Assert.False((cpu.F & Z80.Flags.U) == Z80.Flags.U);
             Assert.True((cpu.F & Z80.Flags.X) == Z80.Flags.X);
+        }
+
+        [Fact]
+        public void XorAccumulatorWithItselfZeroesAccumulator()
+        {
+            var fakeBus = A.Fake<IBus>();
+
+            var program = new Dictionary<ushort, byte>
+            {
+                // Program Code
+                { 0x0080, 0xAF }, // XOR A - a 1-byte way of loading A with zero
+                { 0x0081, 0x00 },
+                { 0x0082, 0x00 },
+                { 0x0083, 0x00 },
+                { 0x0084, 0x00 },
+            };
+
+            A.CallTo(() => fakeBus.Read(A<ushort>._, A<bool>._))
+                .ReturnsLazily((ushort addr, bool ro) => program[addr]);
+
+            var cpu = new Z80() { A = 0x42, PC = 0x0080 };
+            cpu.ConnectToBus(fakeBus);
+
+            cpu.Step();
+
+            Assert.Equal(0x00, cpu.A);
+            Assert.False((cpu.F & Z80.Flags.N) == Z80.Flags.N);
+            Assert.True((cpu.F & Z80.Flags.Z) == Z80.Flags.Z);
+            Assert.False((cpu.F & Z80.Flags.S) == Z80.Flags.S);
+            Assert.False((cpu.F & Z80.Flags.H) == Z80.Flags.H);
+            Assert.True((cpu.F & Z80.Flags.P) == Z80.Flags.P);
+            Assert.False((cpu.F & Z80.Flags.C) == Z80.Flags.C);
+            Assert.False((cpu.F & Z80.Flags.U) == Z80.Flags.U);
+            Assert.False((cpu.F & Z80.Flags.X) == Z80.Flags.X);
         }
     }
 }
