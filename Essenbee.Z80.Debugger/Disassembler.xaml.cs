b6bd6e4d86c6daa1f2f40c24441b7b6cb39eca35
Stu Bonham <sbonham@rics.org>
2019-11-18 13:42:13 +0000

    Break down UI into UserControls

---
+++ Essenbee.Z80.Debugger/Disassembler.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Essenbee.Z80.Debugger
+{
+    /// <summary>
+    /// Interaction logic for Disassembler.xaml
+    /// </summary>
+    public partial class Disassembler : UserControl
+    {
+        public Disassembler()
+        {
+            InitializeComponent();
+        }
+    }
+}
