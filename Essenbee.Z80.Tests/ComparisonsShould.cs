4ee4f8a8626fc5f76b068c6298e2baf364ba6c01
Stuart Bonham <stuart_bonham@blueyonder.co.uk>
2019-12-10 21:53:53 +0000

    Added test for CP A

--- Essenbee.Z80.Tests/ComparisonsShould.cs
+++ Essenbee.Z80.Tests/ComparisonsShould.cs
@@ -7,6 +7,41 @@ namespace Essenbee.Z80.Tests
     public class ComparisonsShould
     {
         [Fact]
+        private void CompareAccumulatorWithItselfWithCPR()
+        {
+            var fakeBus = A.Fake<IBus>();
+
+            var program = new Dictionary<ushort, byte>
+            {
+                // Program Code
+                { 0x0080, 0xBF }, // CP A
+                { 0x0081, 0x00 },
+                { 0x0082, 0x00 },
+                { 0x0083, 0x00 },
+                { 0x0084, 0x00 },
+            };
+
+            A.CallTo(() => fakeBus.Read(A<ushort>._, A<bool>._))
+                .ReturnsLazily((ushort addr, bool ro) => program[addr]);
+
+            var cpu = new Z80() { A = 0xF5, PC = 0x0080 };
+            cpu.ConnectToBus(fakeBus);
+
+            cpu.Step();
+
+            Assert.Equal(0xF5, cpu.A);
+            Assert.True((cpu.F & Z80.Flags.N) == Z80.Flags.N);
+            Assert.True((cpu.F & Z80.Flags.Z) == Z80.Flags.Z);
+            Assert.False((cpu.F & Z80.Flags.S) == Z80.Flags.S);
+            Assert.False((cpu.F & Z80.Flags.H) == Z80.Flags.H);
+            Assert.False((cpu.F & Z80.Flags.P) == Z80.Flags.P);
+            Assert.False((cpu.F & Z80.Flags.C) == Z80.Flags.C);
+            Assert.False((cpu.F & Z80.Flags.U) == Z80.Flags.U);
+            Assert.False((cpu.F & Z80.Flags.X) == Z80.Flags.X);
+        }
+
+
+        [Fact]
         private void SetZeroFlagWhenTrueForCPR()
         {
             var fakeBus = A.Fake<IBus>();
