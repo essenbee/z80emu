be95ac5f7174dbee1b2ab3b8f9cf3d573233e9fe
Stuart Bonham <stuart_bonham@blueyonder.co.uk>
2019-12-21 12:32:20 +0000

    Added BIT b,(IX+d) and BIT b, (IY+d)

--- Essenbee.Z80/Z80.BitGroup.cs
+++ Essenbee.Z80/Z80.BitGroup.cs
@@ -25,8 +25,6 @@
             // Undocumented flags
             SetFlag(Flags.X, ((n & 0x0008) > 0) ? true : false); //Copy of bit 3
             SetFlag(Flags.U, ((n & 0x0020) > 0) ? true : false); //Copy of bit 5
-            //SetFlag(Flags.X, bit == 3 && result != 0);
-            //SetFlag(Flags.U, bit == 5 && result != 0);
             SetFlag(Flags.S, bit == 7 && result != 0);
             SetFlag(Flags.P, result == 0);
 
@@ -42,6 +40,64 @@
         private byte BITHL(byte opCode)
         {
             byte n = Fetch1(CBInstructions);
+
+            var bit = (opCode & 0b00111000) >> 3;
+            var result = n & (byte)(1 << bit);
+
+            SetFlag(Flags.Z, result == 0);
+            SetFlag(Flags.H, true);
+            SetFlag(Flags.N, false);
+
+            // Undocumented flags
+            SetFlag(Flags.X, ((MEMPTR & 0x0800) > 0) ? true : false); //Copy of bit 11
+            SetFlag(Flags.U, ((MEMPTR & 0x2000) > 0) ? true : false); //Copy of bit 13
+            SetFlag(Flags.S, bit == 7 && result != 0);
+            SetFlag(Flags.P, result == 0);
+
+            SetQ();
+
+            return 0;
+        }
+
+        // Instruction    : BIT b,(IX+d)
+        // Operation      : Z <- true if bit is 0
+        // Flags Affected : Z,H,N
+
+        private byte BITIXD(byte opCode)
+        {
+            var d = (sbyte)ReadFromBus((ushort)(PC - 2)); // displacement -128 to +127
+            _absoluteAddress = (ushort)(IX + d);
+            MEMPTR = (ushort)(IX + d);
+            var n = Fetch2(DDCBInstructions);
+
+            var bit = (opCode & 0b00111000) >> 3;
+            var result = n & (byte)(1 << bit);
+
+            SetFlag(Flags.Z, result == 0);
+            SetFlag(Flags.H, true);
+            SetFlag(Flags.N, false);
+
+            // Undocumented flags
+            SetFlag(Flags.X, ((MEMPTR & 0x0800) > 0) ? true : false); //Copy of bit 11
+            SetFlag(Flags.U, ((MEMPTR & 0x2000) > 0) ? true : false); //Copy of bit 13
+            SetFlag(Flags.S, bit == 7 && result != 0);
+            SetFlag(Flags.P, result == 0);
+
+            SetQ();
+
+            return 0;
+        }
+
+        // Instruction    : BIT b,(IY+d)
+        // Operation      : Z <- true if bit is 0
+        // Flags Affected : Z,H,N
+
+        private byte BITIYD(byte opCode)
+        {
+            var d = (sbyte)ReadFromBus((ushort)(PC - 2)); // displacement -128 to +127
+            _absoluteAddress = (ushort)(IY + d);
+            MEMPTR = (ushort)(IY + d);
+            var n = Fetch2(FDCBInstructions);
 
             var bit = (opCode & 0b00111000) >> 3;
             var result = n & (byte)(1 << bit);
