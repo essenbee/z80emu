0242eb06963fe1d19142c5e241b2d439c4a33404
Stuart Bonham <stuart_bonham@blueyonder.co.uk>
2019-12-25 09:32:45 +0000

    Added Essenbee.Z80.Sample project

---
+++ Essenbee.Z80.Sample/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace Essenbee.Z80.Sample
+{
+    class Program
+    {
+        private static Z80 _cpu;
+        private static string _rom = @"..\..\..\ROM\48.rom";
+
+        static void Main(string[] args)
+        {
+            var ram = new byte[65536];
+            Array.Clear(ram, 0, ram.Length);
+            var romData = File.ReadAllBytes(_rom);
+
+            if (romData.Length != 16384)
+            {
+                throw new InvalidOperationException("Not a valid ROM file");
+            }
+
+            Array.Copy(romData, ram, 16384);
+            _cpu = new Z80();
+            IBus simpleBus = new SimpleBus(ram);
+            _cpu.ConnectToBus(simpleBus);
+
+            Console.Clear();
+
+            while (true)
+            {
+                try
+                {
+                    _cpu.Run();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.StackTrace);
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ Essenbee.Z80.Sample/SimpleBus.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Essenbee.Z80.Sample
+{
+    public class SimpleBus : IBus
+    {
+        public bool Interrupt { get; set; }
+        public bool NonMaskableInterrupt { get; set; }
+        public IEnumerable<byte> Data { get; set; } = new List<byte>();
+
+        private byte[] _memory;
+
+        public SimpleBus(byte[] ram)
+        {
+            _memory = ram;
+        }
+
+        public IReadOnlyCollection<byte> RAM
+        {
+            get => _memory;
+        }
+
+        public byte Read(ushort addr, bool ro = false)
+        {
+            return _memory[addr];
+        }
+
+        public byte ReadPeripheral(ushort port)
+        {
+
+            Console.WriteLine($"IN 0x{port:X4}");
+
+            return 0;
+        }
+
+        public void Write(ushort addr, byte data)
+        {
+            _memory[addr] = data;
+        }
+
+        public void WritePeripheral(ushort port, byte data)
+        {
+            Console.WriteLine($"OUT 0x{port:X4}, 0x{data:X2}");
+        }
+    }
+}
