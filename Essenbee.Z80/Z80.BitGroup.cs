6eee347d83013181ddb1cdd5860e50396fc6457c
Stuart Bonham <stuart_bonham@blueyonder.co.uk>
2019-12-27 11:49:34 +0000

    Added RES r, RES (HL), RES (IX+d) and RES (IY+d)

--- Essenbee.Z80/Z80.BitGroup.cs
+++ Essenbee.Z80/Z80.BitGroup.cs
@@ -116,5 +116,92 @@
 
             return 0;
         }
+
+        // Instruction    : RES b,r
+        // Operation      : 
+        // Flags Affected : None
+
+        private byte RESBR(byte opCode)
+        {
+            var src = opCode & 0b00000111;
+            byte n = ReadFromRegister(src);
+
+            var bit = (opCode & 0b00111000) >> 3;
+            n &= (byte)~(byte)(1 << bit);
+            AssignToRegister(src, n);
+
+            ResetQ();
+
+            return 0;
+        }
+
+        // Instruction    : RES b,(HL)
+        // Operation      : 
+        // Flags Affected : None
+
+        private byte RESHL(byte opCode)
+        {
+            byte n = Fetch1(CBInstructions);
+
+            var bit = (opCode & 0b00111000) >> 3;
+            n &= (byte)~(byte)(1 << bit);
+            WriteToBus(HL, n);
+
+            ResetQ();
+
+            return 0;
+        }
+
+        // Instruction    : RES b,(IX+d)
+        // Operation      : 
+        // Flags Affected : None
+
+        private byte RESIXD(byte opCode)
+        {
+            var src = opCode & 0b00000111;
+            var d = (sbyte)ReadFromBus((ushort)(PC - 2)); // displacement -128 to +127
+            _absoluteAddress = (ushort)(IX + d);
+            MEMPTR = (ushort)(IX + d);
+            var n = Fetch2(DDCBInstructions);
+
+            var bit = (opCode & 0b00111000) >> 3;
+            n &= (byte)~(byte)(1 << bit);
+            WriteToBus((ushort)(IX + d), n);
+
+            if (src != 6)
+            {
+                AssignToRegister(src, n);
+            }
+
+            ResetQ();
+
+            return 0;
+        }
+
+        // Instruction    : RES b,(IY+d)
+        // Operation      : 
+        // Flags Affected : None
+
+        private byte RESIYD(byte opCode)
+        {
+            var src = opCode & 0b00000111;
+            var d = (sbyte)ReadFromBus((ushort)(PC - 2)); // displacement -128 to +127
+            _absoluteAddress = (ushort)(IY + d);
+            MEMPTR = (ushort)(IY + d);
+            var n = Fetch2(FDCBInstructions);
+
+            var bit = (opCode & 0b00111000) >> 3;
+            n &= (byte)~(byte)(1 << bit);
+            WriteToBus((ushort)(IY + d), n);
+
+            if (src != 6)
+            {
+                AssignToRegister(src, n);
+            }
+
+            ResetQ();
+
+            return 0;
+        }
     }
 }
