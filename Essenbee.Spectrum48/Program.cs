0f6ffd569f09c3be549cbf48e7696f94b890449c
Stuart Bonham <stuart_bonham@blueyonder.co.uk>
2020-01-18 15:32:50 +0000

    Update screen rendering in Essenbee.Spectrum48 sample program

--- Essenbee.Spectrum48/Program.cs
+++ Essenbee.Spectrum48/Program.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using PixelEngine;
+using System;
 using System.IO;
-using PixelEngine;
 
 namespace Essenbee.Z80.Spectrum48
 {
@@ -8,10 +8,11 @@ namespace Essenbee.Z80.Spectrum48
     {
         private static Z80 _cpu;
         private static string _rom = @"..\..\ROM\48.rom";
-        private float _countdown = 0.0f;
-        private bool _runEmulation = false;
+        private SimpleBus _simpleBus = null;
+        private Point _origin = new Point(0, 0);
         private byte[] _ram;
-        private ushort _screen = 0x4000;
+
+        public Program() => AppName = $"Essenbee.Spectrum48";
 
         public override void OnCreate()
         {
@@ -26,83 +27,36 @@ namespace Essenbee.Z80.Spectrum48
 
             Array.Copy(romData, _ram, 16384);
             _cpu = new Z80();
-            IBus simpleBus = new SimpleBus(_ram);
-            _cpu.ConnectToBus(simpleBus);
+            _simpleBus = new SimpleBus(_ram);
+            _cpu.ConnectToBus(_simpleBus);
         }
 
         public override void OnUpdate(float elapsed)
         {
             Clear(Pixel.Presets.Blue);
 
-            if (GetKey(Key.Space).Pressed)
+            if (Focus)
             {
-                _runEmulation = !_runEmulation;
-            }
-
-            if (_runEmulation)
-            {
-                if (_countdown > 0.0f)
+                if (GetKey(Key.Escape).Pressed)
                 {
-                    _countdown -= elapsed;
-                }
-                else
-                {
-                    _countdown += (1.0f / 60.0f) - elapsed;
-
-                    for (int i = 0; i < 100; i++)
-                    {
-                        _cpu.Step();
-                        Console.Write($"\rPC: {_cpu.PC.ToString("X4")}");
-                    }
-                }
-
-                PaintScreen();
-            }
-        }
-
-        public void PaintScreen()
-        {
-            if (_ram != null && _ram.Length >= _screen + 0x1800)
-            {
-                for (var screenThird = 0; screenThird < 3; ++screenThird)
-                {
-                    for (var charLine = 0; charLine < 8; ++charLine)
-                    {
-                        for (var line = 0; line < 8; ++line)
-                        {
-                            var memOffset = _screen + (screenThird << 11) + (charLine << 8) + (line << 5);
-                            var y = (screenThird * 8 * 8) + (line * 8) + (charLine);
-
-                            for (var character = 0; character < 32; ++character)
-                            {
-                                var c = _ram[memOffset + character]; // Get byte value
-
-                                // Decode bits
-                                for (var bitPos = 0; bitPos < 8; ++bitPos)
-                                {
-                                    var b = 0x1 & (c >> (7 - bitPos));
-
-                                    // ToDo: Add support for colours via the attribute area of RAM...
-                                    if (b != 0)
-                                    {
-                                        Draw((character * 8) + bitPos, y, Pixel.Presets.White);
-                                    }
-                                    else
-                                    {
-                                        Draw((character * 8) + bitPos, y, Pixel.Presets.Blue);
-                                    }
-                                }
-                            }
-                        }
-                    }
+                    _cpu.Reset();
                 }
             }
+
+            while (!_simpleBus.ScreenReady)
+            {
+                _cpu.Step();
+                _simpleBus.RunRenderer();
+            }
+
+            _simpleBus.ScreenReady = false;
+            DrawSprite(_origin, _simpleBus.GetScreen()); // Get buffered sprite representing the screen
         }
 
         static void Main(string[] args)
         {
             var spectrum48 = new Program();
-            spectrum48.Construct(32 * 8, 24 * 8, 4, 4);
+            spectrum48.Construct(32 * 8, 24 * 8, 3, 3);
             spectrum48.Start();
         }
     }
--- Essenbee.Spectrum48/SimpleBus.cs
+++ Essenbee.Spectrum48/SimpleBus.cs
@@ -1,15 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using PixelEngine;
 
 namespace Essenbee.Z80.Spectrum48
 {
     public class SimpleBus : IBus
     {
+        public bool ScreenReady { get; set; }
         public bool Interrupt { get; set; }
         public bool NonMaskableInterrupt { get; set; }
         public IList<byte> Data { get; set; } = new List<byte>();
 
         private byte[] _memory;
+        private int _lineRendered = 0;
+        private int _pixelLine = 0;
+        private int _screenLine = 0;
+        private Sprite _theScreen = new Sprite(32 * 8, 24 * 8);
+        private const ushort _screenStart = 0x4000;     // Beginning of Spectrum video RAM
+        private const ushort _attributeStart = 0x5800;  // 768 bytes of colour attributes
 
         public SimpleBus(byte[] ram)
         {
@@ -28,9 +35,6 @@ namespace Essenbee.Z80.Spectrum48
 
         public byte ReadPeripheral(ushort port)
         {
-            Console.WriteLine();
-            Console.WriteLine($"IN 0x{port:X4}");
-
             return 0;
         }
 
@@ -41,8 +45,73 @@ namespace Essenbee.Z80.Spectrum48
 
         public void WritePeripheral(ushort port, byte data)
         {
-            Console.WriteLine();
-            Console.WriteLine($"OUT 0x{port:X4}, 0x{data:X2}");
+        }
+
+        public Sprite GetScreen()
+        {
+            return _theScreen;
+        }
+
+        public void RunRenderer()
+        {
+            StepRenderer();
+        }
+
+        private void StepRenderer()
+        {
+            var offset = _screenStart + (_pixelLine << 8) + (_screenLine << 5);
+            // Top third of screen
+            var memOffset0 = (0 << 11) + offset;
+            // Middle third of screen
+            var memOffset1 = (1 << 11) + offset;
+            // Bottom third of screen
+            var memOffset2 = (2 << 11) + offset;
+
+            for (var c = 0; c < 32; ++c)
+            {
+                var c0 = _memory[memOffset0 + c]; // Get byte value 0
+                var c1 = _memory[memOffset1 + c]; // Get byte value 1
+                var c2 = _memory[memOffset2 + c]; // Get byte value 2
+
+                // Decode bits
+                for (var bitPos = 0; bitPos < 8; ++bitPos)
+                {
+                    var b0 = 0x1 & (c0 >> (7 - bitPos));
+                    var b1 = 0x1 & (c1 >> (7 - bitPos));
+                    var b2 = 0x1 & (c2 >> (7 - bitPos));
+                    var x = (c * 8) + bitPos;
+
+                    _theScreen[x, _lineRendered + 0] = b0 != 0 
+                        ? Pixel.Presets.White
+                        : Pixel.Presets.Blue;
+                    _theScreen[x, _lineRendered + 64] = b1 != 0
+                        ? Pixel.Presets.White
+                        : Pixel.Presets.Blue;
+                    _theScreen[x, _lineRendered + 128] = b2 != 0
+                        ? Pixel.Presets.White
+                        : Pixel.Presets.Blue;
+                }
+            }
+
+            _lineRendered++;
+            _pixelLine++;
+
+            if (_pixelLine > 7)
+            {
+                _pixelLine = 0;
+                _screenLine++;
+            }
+
+            if (_screenLine > 7)
+            {
+                _screenLine = 0;;
+            }
+
+            if (_lineRendered > 63)
+            {
+                _lineRendered = 0;
+                ScreenReady = true;
+            }
         }
     }
 }
