91df3736799e64ccefd8ce8ef285bcfdbf2f23d5
Stuart Bonham <stuart_bonham@blueyonder.co.uk>
2020-03-15 11:26:43 +0000

    Fix a broken test

--- Essenbee.Z80.Tests/Z80EmulatorShould.cs
+++ Essenbee.Z80.Tests/Z80EmulatorShould.cs
@@ -151,38 +151,6 @@ namespace Essenbee.Z80.Tests
         }
 
         [Fact]
-        private void HandleStrayOpCodes()
-        {
-            var fakeBus = A.Fake<IBus>();
-
-            var program = new Dictionary<ushort, byte>
-            {
-                // Program Code
-                { 0x0080, 0xFD },
-                { 0x0081, 0xDD },
-                { 0x0082, 0x21 }, // LD HL &1000
-                { 0x0083, 0x00 },
-                { 0x0084, 0x10 },
-                { 0x0085, 0x00 },
-                { 0x0086, 0x00 },
-                { 0x0087, 0x00 },
-            };
-
-            A.CallTo(() => fakeBus.Read(A<ushort>._, A<bool>._))
-                .ReturnsLazily((ushort addr, bool ro) => program[addr]);
-
-            var cpu = new Z80() { A = 0x00, H = 0x00, L = 0x00, PC = 0x0080 };
-            cpu.ConnectToBus(fakeBus);
-
-            while (cpu.PC < 0x0085)
-            {
-                cpu.Step();
-            }
-
-            Assert.Equal(0x1000, cpu.HL);
-        }
-
-        [Fact]
         private void HandleStrayOpCodes2()
         {
             var fakeBus = A.Fake<IBus>();
